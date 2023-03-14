787018b671658e67935d24c4bda96420b8778252
Patryk Migaj <77615611+patromi@users.noreply.github.com>
2023-03-15 00:28:51 +0100

    Update settings.py

--- imageapi/imageapi/settings.py
+++ imageapi/imageapi/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'imageapi.urls'
