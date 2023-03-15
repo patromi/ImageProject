6f4fa712c98c44d07ba34adc3daf1e286fc9baa1
admin <patromi123@gmail.com>
2023-03-15 14:02:11 +0100

    refactor + docker fix

--- imageapi/users/management/commands/initadmin.py
+++ imageapi/users/management/commands/initadmin.py
@@ -1,5 +1,8 @@
+import os
+
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.core.management.utils import get_random_secret_key
 from users.models import ImageUser as User
 
 class Command(BaseCommand):
@@ -9,7 +12,7 @@ class Command(BaseCommand):
             email = 'admin@gmail.com'
             password = 'admin'
             print('Creating account for %s ' % (email))
-            admin = User.objects.create_superuser(email=email,password=password)
+            admin = User.objects.create_superuser(email=email,password=password,plan="ROOT")
             admin.is_active = True
             admin.is_admin = True
             admin.save()
